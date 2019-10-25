9b31b9fb247cca679d2e95045e71fb538b47e2c2
Zsolt Nagy <Zsolt.Nagy1@ibm.com>
2019-10-25 15:28:26 +0200

    Update_Notebook_Updating_Loading_Data
    Updating Loading data cells

--- ABC_LLC_Leads.py
+++ ABC_LLC_Leads.py
@@ -25,12 +25,32 @@ import seaborn as sns
 
 # ### <span style="color:dimgray">Loading data</span>
 
+# <b>Features of df_bids:</b><br/>
+# ● created: time of the bid<br />
+# ● leadID: unique ID of lead<br />
+# ● max_bid: bid offered by Abc LLC for that particular lead<br />
+# ● won: 1 if their bid was the highest and they won the bid, 0 otherwise
+
+# In[ ]:
+
+
+# Reading Bids csv file
+df_bids = pd.read_csv('C:/Users/ZsoltNagy/Desktop/github_projects/ABC_LLC_Leads_Boberdoo/ABC_LLC_Leads/Data/bids_hw.csv')
+df_bids.head(5)
+
+
+# <b>Features of df_leads:</b><br />
+# ● leadID: unique ID of the lead<br />
+# ● state_18: the state where the lead lives in<br />
+# ● gender_18: lead's gender, which is 1 if male, and 2 if female<br />
+# ● estimated_household_income_18: this is the lead's estimation of their own household income per year in dollars <br />
+# ● cpa_status_18: 1 for closed leads, 0 for lost leads<br />
+# ● premium_amount_18: value of the contract signed, to be paid monthly in dollars<br />
+
 # In[6]:
 
 
-df_bids = pd.read_csv('C:/Users/ZsoltNagy/Desktop/github_projects/ABC_LLC_Leads_Boberdoo/ABC_LLC_Leads/Data/bids_hw.csv')
-print(df_bids.head(5))
-
+# Reading Leads csv file
 df_leads = pd.read_csv('C:/Users/ZsoltNagy/Desktop/github_projects/ABC_LLC_Leads_Boberdoo/ABC_LLC_Leads/Data/leads_hw.csv')
-print(df_leads.head(5))
+df_leads.head(5)
 
