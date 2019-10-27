e427e661e059e60959d684eceabce8374c757c7d
Zsolt Nagy <Zsolt.Nagy1@ibm.com>
2019-10-27 09:50:11 +0100

    Update_Notebook_Finding_Most_typical_client
    Added categorical features and df about most typical client groups

--- ABC_LLC_Leads.py
+++ ABC_LLC_Leads.py
@@ -57,16 +57,15 @@ df_leads.head(5)
 
 # ### <span style="color:dimgray"> Merging dataset </span>
 
-# In[87]:
+# In[4]:
 
 
+# Joining the two dataframe
 df = df_leads >> left_join(df_bids, by = "leadID")
 df.head(10)
 
 
-# ### <span style="color:dimgray"> Abc LLC's current most typical client </span>
-
-# #### <span style="color:darkgray"> Exploring the dataset </span>
+# ### <span style="color:dimgray"> Exploring the dataset </span>
 
 # In[5]:
 
@@ -82,7 +81,7 @@ df.info()
 df.describe()
 
 
-# In[196]:
+# In[7]:
 
 
 #Missing values computation
@@ -91,25 +90,25 @@ df.isnull().sum()
 
 # #### <span style="color:darkgray"> Univariate analysis on closed leads </span>
 
-# In[180]:
+# In[8]:
 
 
 df_closed = df >> mask(X.cpa_status_18 == 1)
 
 
-# In[181]:
+# In[9]:
 
 
 df_closed.dtypes
 
 
-# In[182]:
+# In[10]:
 
 
 df_closed.describe()
 
 
-# In[183]:
+# In[11]:
 
 
 # Distribution of gender 
@@ -117,73 +116,67 @@ sns.countplot(x='gender_18', data=df_closed, palette="BuPu")
 plt.title('Distribution of Gender')
 
 
-# In[184]:
+# In[12]:
 
 
 sns.boxplot(x="age_18", data=df_closed, orient="v", palette="BuPu")
 
 
-# In[185]:
+# In[27]:
 
 
 # Distribution of ages
 
-df_closed.hist('age_18', bins=30)
+df_closed.hist('age_18', bins=30, color = "lightsteelblue", ec="teal")
 plt.title('Distribution of Age')
 plt.xlabel('Age')
 
 
-# In[195]:
+# In[14]:
 
 
 sns.boxplot(x="estimated_household_income_18", data=df_closed, orient="v", palette="BuPu")
 
 
-# In[188]:
+# In[15]:
 
 
 #Excluding the outlier case 
 df_closed = df_closed >> mask(X.estimated_household_income_18 < 250000)
 
 
-# In[189]:
+# In[28]:
 
 
-df_closed.hist('estimated_household_income_18', bins=20)
+df_closed.hist('estimated_household_income_18', bins=20, color = "lightsteelblue", ec="teal")
 plt.title('Distribution of HH Income')
 plt.xlabel('HH Income')
 
 
-# In[190]:
+# In[17]:
 
 
 sns.boxplot(x="premium_amount_18", data=df_closed, orient="v", palette="BuPu")
 
 
-# In[191]:
+# In[18]:
 
 
 #Excluding the outlier case 
 df_closed = df_closed >> mask(X.premium_amount_18 < max(df_closed['premium_amount_18'])) 
 
 
-# In[194]:
+# In[29]:
 
 
 df_closed = df_closed >> mask(X.premium_amount_18 > 0) 
 
-df_closed.hist(column='premium_amount_18', bins=25)
+df_closed.hist(column='premium_amount_18', bins=25, color = "lightsteelblue", ec="teal")
 plt.title('Value of the contract')
 plt.xlabel('Contract Value')
 
 
-# In[172]:
-
-
-
-
-
-# In[229]:
+# In[20]:
 
 
 # Distribution of closed leads by states 
@@ -198,8 +191,42 @@ g.fig.set_figwidth(10)
 g.fig.set_figheight(7)
 
 
-# In[10]:
+# ### <span style="color:dimgray"> Abc LLC's current most typical client </span>
+
+# In[52]:
 
 
-df >> mask(X.cpa_status_18 == 1) >> group_by(X.gender_18, X.state_18) >> summarize(age = X.age_18.mean(), income = X.estimated_household_income_18.mean(), contr_size = X.premium_amount_18.mean())
+# Creating new categorical age variable
+#bins = pd.IntervalIndex.from_tuples([(0, 17), (18, 44), (45, 64), (65, 150)])
+bins=[0, 18, 45, 65, np.inf]
+df_closed['Age_c'] = pd.cut(round(df_closed['age_18'], 0), bins=bins, labels=['Under_18', '18-44', '45-64', '65_and_Above'])
+
+
+# In[53]:
+
+
+# Checking classes of age
+df_closed >> group_by(X.Age_c) >> summarize(N=n(X.leadID), Min=X.age_18.min(), Max=X.age_18.max())
+
+
+# In[32]:
+
+
+# Creating new categorical income variable
+df_closed['Income_c'] = pd.cut(df_closed['estimated_household_income_18'], bins=4, labels=['Lowest', 'Lower_middle', 'Upper_middle', 'Highest'])
+
+
+# In[39]:
+
+
+# Checking classes of income
+df_closed >> group_by(X.Income_c) >> summarize(N=n(X.leadID), Min=X.estimated_household_income_18.min(), Max=X.estimated_household_income_18.max())
+
+
+# In[60]:
+
+
+#Finding the 5 most typical customer groups
+df_typ_cust = df_closed >> group_by(X.state_18, X.gender_18, X.Age_c, X.Income_c) >> summarize(N = n(X.leadID))
+df_typ_cust.sort_values(by='N', ascending=False).head(5)
 
